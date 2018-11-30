ac6e05e31afb61915f4f307d88fd37a809a062df
Nafeis Weixelman <nafeisw@yahoo.com>
2018-11-29 17:19:18 -0800

    Updated

---
+++ GalagaLite/GalagaLite/Class/Scaling.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System.Numerics;
+
+namespace GalagaLite.Class
+{
+    class Scaling
+    {
+        public static void SetScale()
+        {
+            //Display Information
+            MainPage.scaleWidth = (float)MainPage.bounds.Width / MainPage.DesignWidth;
+            MainPage.scaleHeight = (float)MainPage.bounds.Height / MainPage.DesignHeight;
+        }
+
+        public static Transform2DEffect img(CanvasBitmap source)
+        {
+            Transform2DEffect image;
+            image = new Transform2DEffect() { Source = source };
+            image.TransformMatrix = Matrix3x2.CreateScale(MainPage.scaleWidth, MainPage.scaleHeight);
+            return image;
+        }
+    }
+}
---
+++ GalagaLite/GalagaLite/Class/gsm.cs
@@ -0,0 +1,24 @@
+﻿namespace GalagaLite.Class
+{
+    class gsm
+    {
+        public static void GSM()
+        {
+            if (MainPage.RoundEnded == true)
+            {
+                MainPage.BG = MainPage.ScoreScreen;
+            }
+            else
+            {
+                if (MainPage.GameState == 0)
+                {
+                    MainPage.BG = MainPage.StartScreen;
+                }
+                else if (MainPage.GameState == 1)
+                {
+                    MainPage.BG = MainPage.Level1;
+                }
+            }
+        }
+    }
+}
