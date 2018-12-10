5c0a3fea5cf10606e62931697877b362efd43b33
Nafeis Weixelman <nafeisw@yahoo.com>
2018-12-09 20:30:14 -0800

    Work in progress

--- GalagaLite/GalagaLite/MainPage.xaml.cs
+++ GalagaLite/GalagaLite/MainPage.xaml.cs
@@ -18,21 +18,31 @@ namespace GalagaLite
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public static CanvasBitmap BG, StartScreen, Level1, ScoreScreen, Photon;
+        public static CanvasBitmap BG, StartScreen, Level1, ScoreScreen, Photon, Enemy1, Enemy2, SHIP_IMG, MyShip;
         public static Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
         public static float DesignWidth = 1920;
         public static float DesignHeight = 1080;
         public static float scaleWidth, scaleHeight, pointX, pointY, photonX, photonY;
-        public static int countdown = 6;
+        public static int countdown = 60;
         public static bool RoundEnded = false;
 
         public static int GameState = 0;             //0 refers to StartScreen, 1 to Level1
         public static DispatcherTimer RoundTimer = new DispatcherTimer();
+        public static DispatcherTimer EnemyTimer = new DispatcherTimer();
 
         // Lists (Projectile)
         public static List<float> photonXPOS = new List<float>();
         public static List<float> photonYPOS = new List<float>();
         public static List<float> percent = new List<float>();
+
+        // Lists (Enemies)
+        public static List<float> enemyXPOS = new List<float>();
+        public static List<float> enemyYPOS = new List<float>();
+        public static List<int> enemySHIP = new List<int>();
+
+        //Random Generators
+        public Random EnemyShipRand = new Random();     //ship type
+        public Random EnemyGenRand = new Random();      //generation interval
 
         public MainPage()
         {
@@ -44,6 +54,22 @@ namespace GalagaLite
 
             RoundTimer.Tick += RoundTimer_Tick;
             RoundTimer.Interval = new TimeSpan(0, 0, 1);       //hours, minutes, seconds
+
+            EnemyTimer.Tick += EnemyTimer_Tick;
+            EnemyTimer.Interval = new TimeSpan(0, 0, 0,0, EnemyGenRand.Next(300,3000));
+
+
+        }
+
+        private void EnemyTimer_Tick(object sender, object e)
+        {
+            int ES = EnemyShipRand.Next(1, 3);
+
+            enemyXPOS.Add(50 * scaleWidth);
+            enemyYPOS.Add(119 * scaleHeight);
+            enemySHIP.Add(ES);
+
+            EnemyTimer.Interval = new TimeSpan(0, 0, 0, 0, EnemyGenRand.Next(300, 3000));
         }
 
         private void RoundTimer_Tick(object sender, object e)
@@ -74,6 +100,9 @@ namespace GalagaLite
             Level1 = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/star_background.png"));
             ScoreScreen = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/gameover.png"));
             Photon = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/laser.png"));
+            MyShip = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/spaceship.png"));
+            Enemy1 = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/alien.png"));
+            Enemy2 = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/alien2.png"));
         }
 
         private void GameCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
@@ -82,20 +111,34 @@ namespace GalagaLite
             args.DrawingSession.DrawImage(Scaling.img(BG));
             args.DrawingSession.DrawText(countdown.ToString(), 120, 120, Colors.White);
 
-            //Display Projectile
-            for(int i =0; i<photonXPOS.Count; i++)
-            {
-                pointX = (photonX + (photonXPOS[i] - photonX) * percent[i]);
-                pointY = (photonY + (photonYPOS[i] - photonY) * percent[i]);
-                args.DrawingSession.DrawImage(Scaling.img(Photon), pointX - (20 * scaleWidth), pointY - (20 * scaleHeight));
+            if(GameState > 0)
+            { 
+                args.DrawingSession.DrawImage(Scaling.img(MyShip),(float)bounds.Width / 2 - (64 * scaleWidth), (float)bounds.Height - (140 * scaleHeight));
 
-                percent[i] += (0.1f) * scaleHeight;
-
-                if(pointY < 0f)
+                //Displaying Enemies
+                for (int j = 0; j < enemyXPOS.Count; j++)
                 {
-                    photonXPOS.RemoveAt(i);
-                    photonYPOS.RemoveAt(i);
-                    percent.RemoveAt(i);
+                    if (enemySHIP[j] == 1) { SHIP_IMG = Enemy1; }
+                    if (enemySHIP[j] == 2) { SHIP_IMG = Enemy2; }
+                    enemyXPOS[j] += 3;
+                    args.DrawingSession.DrawImage(Scaling.img(SHIP_IMG), enemyXPOS[j], enemyYPOS[j]);
+                }
+
+                //Display Projectile
+                for (int i = 0; i < photonXPOS.Count; i++)
+                {
+                    pointX = (photonX + (photonXPOS[i] - photonX) * percent[i]);
+                    pointY = (photonY + (photonYPOS[i] - photonY) * percent[i]);
+                    args.DrawingSession.DrawImage(Scaling.img(Photon), pointX - (41 * scaleWidth), pointY - (63 * scaleHeight));
+
+                    percent[i] += (0.025f) * scaleHeight;
+
+                    if (pointY < 0f)
+                    {
+                        photonXPOS.RemoveAt(i);
+                        photonYPOS.RemoveAt(i);
+                        percent.RemoveAt(i);
+                    }
                 }
             }
 
@@ -108,7 +151,13 @@ namespace GalagaLite
             {
                 GameState = 0;
                 RoundEnded = false;
-                countdown = 6;
+                countdown = 60;
+
+                //Stop Enemy Timer
+                EnemyTimer.Stop();
+                enemyXPOS.Clear();
+                enemyYPOS.Clear();
+                enemySHIP.Clear();
             }
             else
             {
@@ -116,6 +165,7 @@ namespace GalagaLite
                 {
                     GameState += 1;
                     RoundTimer.Start();
+                    EnemyTimer.Start();
 
                 }
                 else if (GameState > 0)
