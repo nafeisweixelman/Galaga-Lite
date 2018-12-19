9cf643094cdb74e97583d1385019ef703c845e1f
Michael Hill <mhhillsd@gmail.com>
2018-12-18 20:58:02 -0800

    commenting

--- GalagaLite/GalagaLite/Class/Alien.cs
+++ GalagaLite/GalagaLite/Class/Alien.cs
@@ -28,6 +28,9 @@ namespace GalagaLite.Class
             AlienXPOS = 0;
             AlienYPOS = 0;
         }
+        /// <summary>
+        /// function to create aliens
+        /// </summary>
         public static void createAliens()
         {
             for (int a = 0; a < GSM.holdEnemies; a++)
@@ -35,7 +38,6 @@ namespace GalagaLite.Class
                 if (GSM.totalEnemies > 0)
                 {
                     Alien myAlien = new Alien((float)(MainPage.bounds.Width / 2) - (GSM.totalEnemies * 45) + (a * 45 * MainPage.scaleHeight), (50 + MainPage.scaleHeight), 1); 
-                    //Alien myAlien = new Alien((90 * a * MainPage.scaleHeight), (50 + MainPage.scaleHeight), 1);
                     MainPage.alienList.Add(myAlien);
                 }
 
@@ -69,41 +71,48 @@ namespace GalagaLite.Class
             }
 
         }
+
         /// <summary>
         /// Move function for alien class to prevent any aliens moving off the screen
         /// </summary>
-
         public void MoveAlien()
         {
-            AlienXPOS += fleetDIR;
+            AlienXPOS += fleetDIR; //moves alien in direction of fleet
+
+            //if alien is above the set position then moves alien down into it
             if (AlienYPOS <= SetYPOS - 5)
             {
                 AlienYPOS += 2;
             }
+            //if alien is below set position then moves down and sets it in attack
             else if(AlienYPOS >= SetYPOS + 5)
             {
                 if(AlienXPOS < 0)
                 {
                     AlienXPOS += 5;
                 }
-                AlienYPOS += alienDown;
-                AlienXPOS += fleetDIR * 5;
+                AlienYPOS += alienDown; //alien moves down at variable speed set by level
+                AlienXPOS += fleetDIR * 5; // alien moves side to side more when attacking
             }
+            //if alien is in bounds it goes to setYPOS
             else
             {
                 AlienYPOS = SetYPOS;
             }
+            //if alien goes off screen at bottom it resets up top
             if (AlienYPOS > MainPage.bounds.Height)
             {
                 AlienYPOS = -40;
                 AlienXPOS = SetXPOS + fleetPOS;
                 attacked = false;
             }
+            //if alien is quarter way down screen then it shoots laser
             if(AlienYPOS > MainPage.bounds.Height / 4 && !attacked)
             {
                 attack();
                 attacked = true;
             }
+            //moving alien projectile and deleting it off screen
             for(int a = 0; a < shootXPOS.Count; a++)
             {
                 shootYPOS[a] += 5;
@@ -115,6 +124,9 @@ namespace GalagaLite.Class
             }
             
         }
+        /// <summary>
+        /// moves fleet between 0 and 41
+        /// </summary>
         public void MoveFleet()
         {
             fleetPOS += fleetDIR;
@@ -133,7 +145,10 @@ namespace GalagaLite.Class
             shootXPOS.Add(AlienXPOS + (35 * MainPage.scaleWidth));
             shootYPOS.Add(AlienYPOS);
         }
-
+        /// <summary>
+        /// returns shootXPOS list
+        /// </summary>`
+        /// <returns></returns>
         public List<float> getShootX()
         {
             return shootXPOS;
@@ -142,7 +157,10 @@ namespace GalagaLite.Class
         {
             return shootYPOS;
         }
-
+        /// <summary>
+        /// removes alien laser at 
+        /// </summary>
+        /// <param name="a"></param>
         public void removeShoot(int a)
         {
             shootXPOS.RemoveAt(a);
